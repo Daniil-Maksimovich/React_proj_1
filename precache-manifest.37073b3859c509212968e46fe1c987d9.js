41b82610bced9c69f8a3773fbc716e544a407549
Daniil-Maksimovich <daniilmaksimovich222@gmail.com>
2020-10-07 10:02:48 +0300

    Updates

---
+++ precache-manifest.37073b3859c509212968e46fe1c987d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fc94becb9c77ccf8bd043275122011",
+    "url": "/React_proj_1/home/index.html"
+  },
+  {
+    "revision": "ac6c28b5449854f619af",
+    "url": "/React_proj_1/home/static/css/main.1391ac36.chunk.css"
+  },
+  {
+    "revision": "44eefc794ff3217bb396",
+    "url": "/React_proj_1/home/static/js/2.99d6bf83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_proj_1/home/static/js/2.99d6bf83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6c28b5449854f619af",
+    "url": "/React_proj_1/home/static/js/main.c1d52823.chunk.js"
+  },
+  {
+    "revision": "68f2c8a33893ec12e47c",
+    "url": "/React_proj_1/home/static/js/runtime-main.8888a8f0.js"
+  },
+  {
+    "revision": "21bb70b62317f276f2e97a919ff5bd8c",
+    "url": "/React_proj_1/home/static/media/BebasNeue-Regular.21bb70b6.ttf"
+  },
+  {
+    "revision": "950f66714bda7d9dc2f59a48e8705f17",
+    "url": "/React_proj_1/home/static/media/Dan.950f6671.jpg"
+  },
+  {
+    "revision": "4c07a70e0e5ab9775f3c614e9534dd0d",
+    "url": "/React_proj_1/home/static/media/Layer.4c07a70e.png"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/React_proj_1/home/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "c8f6349f467304e59feaad33c461cc80",
+    "url": "/React_proj_1/home/static/media/Maxim.c8f6349f.jpg"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/React_proj_1/home/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/React_proj_1/home/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/React_proj_1/home/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
