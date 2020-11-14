bae77772279eaa0ceffa33e55e660526c4af9f77
Daniil-Maksimovich <daniilmaksimovich222@gmail.com>
2020-11-15 00:09:50 +0200

    new name

--- src/components/wrappers/App.js
+++ src/components/wrappers/App.js
@@ -31,7 +31,7 @@ const App = () => {
           <link rel="icon" href={logo}/>
         </Helmet>
         <Header>
-          {/* <Redirect from="/" to="/home"/> */}
+          <Redirect from="/" to="/home"/>
           <Switch>
             <Route path="/home" component={Home}/>
             <Route exact path="/sales" component={Sales}/>
